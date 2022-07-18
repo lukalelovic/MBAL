440c78916b3f7be697ca2147a1ec4f5de8f0c1db
lukalelovic <lukalelovic@gmail.com>
2022-07-18 15:37:30 +0200

    database type representation

--- Diagrams/test.py
+++ Diagrams/test.py
@@ -1,5 +1,6 @@
 from diagrams import Diagram
 from diagrams.aws.compute import EC2
+from diagrams.aws.database import RDS
 
 import json
 
@@ -9,13 +10,20 @@ data = json.load(f)
 
 nodes = data['nodes']
 
-nodeMap = {}
-
 # Generate diagram based on node map
 with Diagram(data['systemName'] + '-' + data['systemVersion']):
+    # Format node with type
+    def formatNode(node):
+        if (node['nodeType'] == 'database'):
+            return RDS(node['nodeName'])
+        
+        return EC2(node['nodeName'])
+
+
     # Fill node map with node ID as key, node name as value
+    nodeMap = {}
     for node in nodes:
-        nodeMap.update({ node['nodeID']: EC2(node['nodeName']) })
+        nodeMap.update({ node['nodeID']: formatNode(node) })
 
     # Iterate through all nodes
     for node in nodes:
--- README.md
+++ README.md
@@ -4,4 +4,4 @@ JSON-based domain-specific language for describing MSA-based systems
 
 Currently coupled with the diagrams Python library for generating a graphical model from the language
 
-To setup, clone the repo, navigate to Diagrams/ and do ```pip install requirements.txt``` to get the Python project setup.
+To setup, clone the repo, navigate to Diagrams/ and do ```pip install -r requirements.txt``` to get the Python project setup.
