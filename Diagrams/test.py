38882ad0bde67eb16579e29b9710675e1a0868c7
MikeyMike7777 <michaelm246810@gmail.com>
2022-07-20 11:16:20 +0200

    Updated nodes icons

--- Diagrams/test.py
+++ Diagrams/test.py
@@ -1,15 +1,21 @@
 from modulefinder import EXTENDED_ARG
 from diagrams import Diagram, Edge
-from diagrams.aws.compute import EC2, Lambda
+from diagrams.aws.compute import EC2
 from diagrams.aws.database import RDS
-from diagrams.aws.database import RDS
-from diagrams.aws.storage import S3
+from diagrams.aws.storage import SimpleStorageServiceS3Bucket
 from diagrams.aws.integration import SQS
+from diagrams.aws.analytics import DataPipeline
+from diagrams.aws.general import GenericDatabase, InternetGateway, User, Toolkit
+from diagrams.aws.network import APIGateway, APIGatewayEndpoint
+from diagrams.aws.analytics import ManagedStreamingForKafka
+from diagrams.aws.enablement import ManagedServices, ProfessionalServices
+from diagrams.aws.storage import S3GlacierArchive
+from diagrams.aws.management import Config, ManagedServices as MS
 
 import json
 
 # Open JSON file
-f = open('../example.json')
+f = open('../Extensions/exFullSystem.json')
 data = json.load(f)
 
 nodes = data['nodes']
@@ -18,14 +24,41 @@ nodes = data['nodes']
 with Diagram(data['systemName'] + '-' + data['systemVersion']):
     # Format node with type
     def formatNode(node):
-        if (node['nodeType'] == 'handler'):
-            return Lambda(node['nodeName'])
+        if (node['nodeType'] == 'pipeline'):
+            return DataPipeline(node['nodeName'])
 
-        if (node['nodeType'] == 'storage'):
-            return S3(node['nodeName'])
+        if (node['nodeType'] == 'database'):
+            return GenericDatabase(node['nodeName'])
 
-        if (node['nodeType'] == 'processor'):
-            return SQS(node['nodeName'])
+        if (node['nodeType'] == 'proxy'):
+            return APIGateway(node['nodeName'])
+
+        if (node['nodeType'] == 'service'):
+            return ManagedServices(node['nodeName'])   
+            
+        if (node['nodeType'] == 'bucket'):
+            return SimpleStorageServiceS3Bucket(node['nodeName'])    
+
+        if (node['nodeType'] == 'kafka'):
+            return ManagedStreamingForKafka(node['nodeName'], color = "black")   
+
+        if (node['nodeType'] == 'config'):
+            return Config(node['nodeName'])   
+
+        if (node['nodeType'] == 'API'):
+            return APIGatewayEndpoint(node['nodeName'])        
+            
+        if (node['nodeType'] == 'writer'):
+            return MS(node['nodeName'])
+            
+        if (node['nodeType'] == 'customer'):
+            return ProfessionalServices(node['nodeName'])                                      
+
+        if (node['nodeType'] == 'archive'):
+            return S3GlacierArchive(node['nodeName'])
+
+        if (node['nodeType'] == 'srcSink'):
+            return User(node['nodeName'])    
 
         return EC2(node['nodeName'])
 
